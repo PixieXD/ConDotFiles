8dc7e081af23a06b18fb0c87d7ba837859575293
PixieXD <tikt9440@gmail.com>
2025-10-19 00:28:55 +0800

    whoops i forgot someone

---
+++ Config/Fastfetch/quotes.sh
@@ -0,0 +1,19 @@
+
+quotes=(
+    # fucking bake fucking no fucking hana's fucking lyrics
+    "It's as if a flower of transformation has bloomed."
+    "Well, it's a displeasure to meet you~"
+    "What's with those eyes? Ah, of course it's those eyes-!"
+    "This flower of transformation will soon disappear here."
+    "I can't say anything, but I can't erase it."
+    "It's as if I am made to end the curse of being born."
+
+    # my own thoughts and quotes, pretty huh?
+    "You know, back in my days..."
+    ":kannasip:"
+    "How's your day lately?"
+    "Milo ais satu ye bang~"
+    "Today's Homework~!"
+)
+
+echo -e "${quotes[$((RANDOM % ${#quotes[@]}))]}"
