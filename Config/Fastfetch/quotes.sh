b36f10814f40b14261fdfb708b7b4a5805386dfd
PixieXD <tikt9440@gmail.com>
2025-10-19 16:52:05 +0800

    small change

--- Config/Fastfetch/quotes.sh
+++ Config/Fastfetch/quotes.sh
@@ -1,19 +1,26 @@
+# GUIDE
+
+# it should be like this for color '\033[38;2;<r>;<g>;<b>2m<text>\033[m'
+
+# END OF GUIDE
 
 quotes=(
     # fucking bake fucking no fucking hana's fucking lyrics
-    "It's as if a flower of transformation has bloomed."
-    "Well, it's a displeasure to meet you~"
-    "What's with those eyes? Ah, of course it's those eyes-!"
-    "This flower of transformation will soon disappear here."
-    "I can't say anything, but I can't erase it."
-    "It's as if I am made to end the curse of being born."
+    "\033[38;2;255;112;1162mIt's as if a flower of transformation has bloomed.\033[m"
+    "\033[38;2;200;183;2552mWell, it's a displeasure to meet you~\033[m"
+    "\033[38;2;255;112;1162mWhat's with those eyes? Ah, of course it's those eyes-!\033[m"
+    "\033[38;2;221;108;1082mThis flower of transformation will soon disappear here.\033[m"
+    "\033[38;2;255;112;1162mI can't say anything, but I can't erase it.\033[m"
+    "\033[38;2;255;112;1162mIt's as if I am made to end the curse of being born.\033[m"
 
     # my own thoughts and quotes, pretty huh?
     "You know, back in my days..."
     ":kannasip:"
     "How's your day lately?"
-    "Milo ais satu ye bang~"
+    "\"Milo ais satu ye bang~\""
     "Today's Homework~!"
 )
 
-echo -e "${quotes[$((RANDOM % ${#quotes[@]}))]}"
+tput bold
+tput blink
+echo -e "---- ${quotes[$((RANDOM % ${#quotes[@]}))]} ----"
