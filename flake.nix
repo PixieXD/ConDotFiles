975cfc5c9399de1cc54838c9ca8aa759630fbe64
PixieXD <tikt9440@tutamail.com>
2026-03-10 17:23:10 +0800

    Added cache/pre-built for cachy kernel

--- flake.nix
+++ flake.nix
@@ -36,6 +36,10 @@
                         { pkgs, ... } : {
                             nixpkgs.overlays = [ nix-cachyos-kernel.overlays.pinned ];
                             boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-lts-lto;
+                            {
+                                nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
+                                nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
+                            }
                         }
                     )
                 ];
