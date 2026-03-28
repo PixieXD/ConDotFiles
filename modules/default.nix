b3ca5fb79c48b446920575a76ce022e40a7dd055
PixieXD <tikt9440@tutamail.com>
2026-03-29 02:06:31 +0800

    the 2 L's (imported ly and limine nix files)

---
+++ modules/default.nix
@@ -0,0 +1,12 @@
+{
+  imports = [
+    ./configs/fastfetch
+    ./configs/hyprland
+    ./configs/alacritty.nix
+    ./configs/flameshot.nix
+    ./configs/waybar.nix
+    ./configs/zsh.nix
+    ./configs/limine.nix
+    ./configs/ly.nix
+  ];
+}
