ee20fc23e6b6c1bc00938bc231507c75b530a4f1
PixieXD <tikt9440@tutamail.com>
2026-04-04 18:23:57 +0800

    moved alacritty into it's own folder

---
+++ modules/configs/alacritty/default.nix
@@ -0,0 +1,45 @@
+{ lib, username, pkgs, ... }:
+
+{
+  hjem.users.${username}.xdg.config.files = {
+    "alacritty/alacritty.toml" = {
+      generator = (pkgs.formats.toml {}).generate "alacritty.toml";
+
+      value = {
+        general.import = [ "~/.config/alacritty/style.toml" ];
+        window.padding = { x = 10; y = 10; };
+        font = {
+          size = 15;
+
+          bold.family = "JetBrainsMono Nerd Font";
+          bold.style = "Bold";
+
+          normal.family = "JetBrainsMono Nerd Font";
+          normal.style = "Regular";
+
+          italic.family = "JetBrainsMono Nerd Font";
+          italic.style = "Italic";
+
+          bold_italic.family = "JetBrainsMono Nerd Font";
+          bold_italic.style = "Bold Italic";
+        };
+
+        cursor = {
+          unfocused_hollow = false;
+          style = { shape = "Beam"; blinking = "Always"; };
+        };
+
+        terminal.osc52 = "CopyPaste";
+        mouse.hide_when_typing = true;
+
+        keyboard = {
+          bindings = [
+            { key = "-"; mods = "Control"; action = "DecreaseFontSize"; }
+            { key = "="; mods = "Control"; action = "IncreaseFontSize"; }
+            { key = "V"; mods = "Control"; action = "Paste"; }
+          ];
+        };
+      };
+    };
+  };
+}
