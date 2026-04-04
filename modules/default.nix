0e185e296d091d6d2db42c3901134c99afb975a4
PixieXD <tikt9440@tutamail.com>
2026-04-05 03:32:57 +0800

    added wleave and zsh.nix

--- modules/default.nix
+++ modules/default.nix
@@ -3,6 +3,8 @@
     ./configs/fastfetch
     ./configs/hyprland
     ./configs/alacritty
+    ./configs/wleave
+    ./configs/zsh.nix
     ./configs/flameshot.nix
     ./configs/waybar.nix
     ./configs/limine.nix
