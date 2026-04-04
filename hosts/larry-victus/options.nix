175f47eb8da11cde38cf05ae4e4a1e1c6fb1a078
PixieXD <tikt9440@tutamail.com>
2026-04-05 03:31:46 +0800

    - xdg portal option added - moved some limine options

--- hosts/larry-victus/options.nix
+++ hosts/larry-victus/options.nix
@@ -20,12 +20,34 @@
   boot.loader = {
     limine = {
       enable = true;
-      enableEditor = true;
       maxGenerations = 5;
-      efiSupport = true;
+
+      extraEntries = ''
+          /Windows 11
+            protocol: efi
+            path: uuid(bfd3a7a4-d120-437b-899b-0549f3d725c0):/EFI/Microsoft/Boot/bootmgfw.efi
+      '';
     };
 
     efi.canTouchEfiVariables = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true; # see 160923 or 330916 on nixpkgs' issue tracker for more info
+    extraPortals = with pkgs; [ 
+      kdePackages.xdg-desktop-portal-kde
+      xdg-desktop-portal-gtk
+      ];
+    config = {
+      common.default = [ "gtk" ];
+      gtk.default = [ "gtk" ];
+
+      hyprland = {
+        default = [ "hyprland" "gtk" ];
+        "org.freedesktop.impl.portal.FileChooser" = [ "kde" ];
+      };
+    };
   };
 
   fonts = {
