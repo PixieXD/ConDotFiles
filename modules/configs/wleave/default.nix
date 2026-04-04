ed42193f78b7996c1fd3e4a7f2121cec9a026d9f
PixieXD <tikt9440@tutamail.com>
2026-04-05 03:30:57 +0800

    wleave configuration

--- modules/configs/wleave/default.nix
+++ modules/configs/wleave/default.nix
@@ -5,64 +5,92 @@
     "wleave/layout.json" = {
       generator = lib.generators.toJSON {};
       value = {
+        no-version-info = true;
+        buttons-per-row = "1/1";
+        show-keybinds = true;
+        margin-top = 360;
+        margin-bottom = 360;
         buttons = [
           {
             label = "shutdown";
             action = "systemctl poweroff";
             text = "Shutdown";
-            icon = "./icons/shutdown.png";
+            keybind = "q";
+            icon = ./icons/shutdown.png;
           }
 
           {
             label = "restart";
             action = "systemctl reboot";
             text = "Restart";
-            icon = "./icons/restart.png";
+            keybind = "w";
+            icon = ./icons/restart.png;
           }
 
           {
             label = "logout";
             action = "hyprctl dispatch exit";
             text = "Logout";
-            icon = "./icons/logout.png";
+            keybind = "e";
+            icon = ./icons/logout.png;
           }
-
         ];
       };
     };
 
-    "wleave/style.css".text =
-      ''
-        @import url("./colors_wleave.css");
+    "wleave/style.css".text = ''
+      /**
+       * See https://gnome.pages.gitlab.gnome.org/libadwaita/doc/main/css-variables.html for more CSS variables.
+       * Or use your own colors. Both work, but using your own colors may break the automatic light/dark theme function.
+       */
 
-        window {
-        	font-family: monospace;
-        	font-size: 20pt;
-        	color: #c0caf5;
-        	background-color: @background;
-        }
 
-        button {
-        	border-radius: 10px;
-        	background-repeat: no-repeat;
-        	background-position: center;
-        	background-size: 50%;
-        	border: none;
-        	background-color: transparent;
-        	margin: 5px;
-        	transition:
-        		box-shadow 0.1s ease-in-out,
-        		background-color 0.1s ease-in-out;
-        }
+      window {
+          background-color: rgba(12, 12, 12, 0.8);
+      }
 
-        button:hover {
-        	background-color: @color0;
-        }
+      button {
+          color: #DDAACC;
+          background-color: var(--view-bg-color);
+          border: none;
+          padding: 10px;
+      }
 
-        button:focus {
-        	background-color: @color1;
-        	color: @foreground;
-        }
-      '';
+      button label.action-name {
+          font-size: 24px;
+          font-weight: 400;
+      }
+
+      button label.keybind {
+          font-size: 20px;
+          font-family: monospace;
+      }
+
+      button:hover label.keybind, button:focus label.keybind {
+          opacity: 1;
+      }
+
+      button:focus,
+      button:hover {
+          background-color: color-mix(in srgb, var(--accent-bg-color), var(--view-bg-color));
+      }
+
+      button:active {
+          color: var(--accent-fg-color);
+          background-color: var(--accent-bg-color);
+      }
+
+      button#shutdown {
+          --view-fg-color: #ff8d8d;
+      }
+
+      button#reboot {
+          --view-fg-color: #84ffaa;
+      }
+
+      button#logout {
+          --view-fg-color: #ffcca8;
+      }
+    '';
   };
 }
