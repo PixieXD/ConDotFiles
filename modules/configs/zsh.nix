8e32fcce286b9afd10b5858745e2d807d9ec8a85
PixieXD <tikt9440@tutamail.com>
2026-04-12 19:03:44 +0800

    zsh : removed unused aliases

--- modules/configs/zsh.nix
+++ modules/configs/zsh.nix
@@ -11,10 +11,5 @@
       theme = "robbyrussell";
       plugins = [ "git" ];
     };
-
-    shellAliases = {
-      rebuild = "sudo nixos-rebuild switch --flake /home/larry/Important/ConDotFiles/#larry-victus";
-      rebuild-trace = "sudo nixos-rebuild switch --flake /home/larry/Important/ConDotFiles/#larry-victus --show-trace";
-    };
   };
 }
