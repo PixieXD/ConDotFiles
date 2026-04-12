e8771d5f6ea954bae76d107465f52afa7d83e5d4
PixieXD <tikt9440@tutamail.com>
2026-04-12 19:01:07 +0800

    neovim : added nvf (finally)

--- modules/configs/nvim.nix
+++ modules/configs/nvim.nix
@@ -4,23 +4,62 @@
       vim = {
         viAlias = true;
         vimAlias = true;
+        presence.neocord.enable = true;
+        telescope.enable = true;
 
-        theme.enable = true;
-        theme.name = "catppuccin";
-        theme.style = "darker";
+        options = {
+          tabstop = 2;
+          shiftwidth = 2;
+        };
 
-        lsp.enable = true;
+        lsp = {
+          enable = true;
+          formatOnSave = true;
+          trouble.enable = true;
+          lspSignature.enable = true;
+          lightbulb.enable = true;
+        };
 
-        languages.nix.enable = true;
-        languages.nix.extraDiagnostics.enable = true;
-        languages.nix.format.enable = true;
-        languages.nix.lsp.enable = true;
-        languages.nix.treesitter.enable = true;
+        visuals = {
+          nvim-web-devicons.enable = true;
+          nvim-cursorline.enable = true;
+          fidget-nvim.enable = true;
+          highlight-undo.enable = true;
+          indent-blankline.enable = true;
+        };
 
+        theme = {
+          enable = true;
+          transparent = true;
+          name = "catppuccin";
+          style = "mocha";
+        };
 
-        lazy.enable = true;
+        languages = {
+          enableFormat = true;
+          enableTreesitter = true;
+          enableExtraDiagnostics = true;
+
+          nix.enable = true;
+          css.enable = true;
+          java.enable = true;
+          markdown.enable = true;
+          bash.enable = true;
+          json.enable = true;
+          toml.enable = true;
+        };
+
+        ui = {
+          noice.enable = true;
+          colorizer.enable = true;
+          illuminate.enable = true;
+          breadcrumbs = {
+            enable = true;
+            navbuddy.enable = true;
+          };
+          fastaction.enable = true;
+        };
       };
-
     };
   };
 }
